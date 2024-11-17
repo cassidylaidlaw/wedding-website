03e03c21acde56092dceabd887a781e07ef4b6f1
Cassidy Laidlaw <cassidy256@gmail.com>
2024-11-17 09:43:10 -0800

    Change eslint config to use double quotes

--- game/eslint.config.js
+++ game/eslint.config.js
@@ -1,23 +1,23 @@
 
-import globals from 'globals';
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     eslintConfigPrettier,
     {
-        'languageOptions': {
+        "languageOptions": {
             globals: {
                 ...globals.browser,
                 ...globals.node,
             },
         },
-        'rules': {
-            'semi': ['error', 'always'],
-            'quotes': ['error', 'single']
+        "rules": {
+            "semi": ["error", "always"],
+            "quotes": ["error", "double"]
         }
     }
 ];
